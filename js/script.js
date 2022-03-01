9624bb0a2b32ce396596f816ece4e851ae19875f
Torikul <toriqulislam142@gmail.com>
2022-03-01 17:03:26 +0600

    added close button and sensors info

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 document.getElementById('search-button').addEventListener('click', function () {
    
-    const detailsContainer = document.getElementById('details-container');
-   
+    document.getElementById('spinner').style.display = 'block'
     const searchInput = document.getElementById('search-input');
     const inputValue = searchInput.value;
     const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`
@@ -27,26 +26,32 @@ const getPhones = (phones) => {
     </div>
         `
         phoneContainer.appendChild(div)
+        document.getElementById('spinner').style.display = 'none'
         
 
    })
 }
 const showPhoneInfo = (phoneInfo) => {
+    document.getElementById('spinner').style.display = 'block'
     const url = `https://openapi.programming-hero.com/api/phone/${phoneInfo}`
     fetch(url)
         .then(res => res.json())
         .then(phoneData => singlePhoneInfo(phoneData.data))
-    
-        const detailsContainer = document.getElementById('details-container');
-         detailsContainer.style.display='block'
-
+    const container = document.getElementById('details-container');
+    container.style.display = 'block'
+    container.innerHTML = ''
+   
 }
 
 
 const singlePhoneInfo = (phoneInfo) => {
+   
+
+
     const detailsContainer = document.getElementById('details-container');
     const div = document.createElement('div');
     div.innerHTML = `
+    <img onclick="closeContainer()" id="close-button" src="./images/close.png" alt="">
    <div class="phone-image"> <img src="${phoneInfo.image}" alt=""></div>
     <h5 id="release">${phoneInfo.releaseDate}</h5>
 
@@ -56,24 +61,49 @@ const singlePhoneInfo = (phoneInfo) => {
     <h3 class="features"> Main Feature : </h3>
 
     <div class="feature">
-     <h5><span>Storage</span> : ${phoneInfo.mainFeatures.storage}</h5>
-     <h5><span>Displaysize</span> : ${phoneInfo.mainFeatures.displaySize}</h5>
-     <h5><span>Chipset</span> : ${phoneInfo.mainFeatures.chipSet}</h5>
-     <h5><span>Memory</span> :  ${phoneInfo.mainFeatures.memory}</h5> 
+     <p><span>Storage</span> : ${phoneInfo.mainFeatures.storage}</p>
+     <p><span>Displaysize</span> : ${phoneInfo.mainFeatures.displaySize}</p>
+     <p><span>Chipset</span> : ${phoneInfo.mainFeatures.chipSet}</p>
+     <p><span>Memory</span> :  ${phoneInfo.mainFeatures.memory}</p> 
+ 
     </div>
     
     <h3 class="features"> Others :</h3>
     <div class="feature">
-        <h5> <span>WLAN :</span>${phoneInfo.others.WLAN}</h5>
-        <h5><span>Bluetooth :</span>${phoneInfo.others.Bluetooth}{</h5>
-        <h5><span>GPS :</span>${phoneInfo.others.GPS}</h5>
-        <h5><span>NFC :</span>${phoneInfo.others.NFC}</h5>
-        <h5><span>Radio :</span>${phoneInfo.others.Radio}</h5>
-        <h5><span>USB :</span>${phoneInfo.others.USB}</h5>
+        <p> <span>WLAN :</span>${phoneInfo.others.WLAN}</p>
+        <p><span>Bluetooth :</span>${phoneInfo.others.Bluetooth}{</p>
+        <p><span>GPS :</span>${phoneInfo.others.GPS}</p>
+        <p><span>NFC :</span>${phoneInfo.others.NFC}</p>
+        <p><span>Radio :</span>${phoneInfo.others.Radio}</p>
+        <p><span>USB :</span>${phoneInfo.others.USB}</p>
     </div>
+
+    <h3 class="features"> Sensors :</h3>
+    
+
+ 
     `
     detailsContainer.appendChild(div);
-   
- 
- 
+    document.getElementById('spinner').style.display = 'none'
+        
+phoneInfo.mainFeatures.sensors.forEach(sensor => {
+    const detailsContainer = document.getElementById('details-container');
+    const span = document.createElement('span'); 
+    span.innerHTML = `
+    
+    <span class="sensors">${sensor} ,</span>
+    `
+    detailsContainer.appendChild(span)
+})
+    
+    const buttonDiv = document.createElement('div');
+    buttonDiv.innerHTML = `
+    <button onclick="closeContainer()" class = "buy-button">Buy Now</button>
+    <button onclick="closeContainer()" class = "cancel-button">Cancel</button>
+    `
+    detailsContainer.appendChild(buttonDiv)
+}
+
+const closeContainer = ()=> {
+    document.getElementById('details-container').style.display = 'none'
 }
